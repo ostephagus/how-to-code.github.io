4b5a14b82d035b53a91fe9a86708a2fe4c0eaf32
Sam <37499766+ostephagus@users.noreply.github.com>
2018-05-14 11:10:58 +0100

    Create sam.js

---
+++ js-files/sam.js
@@ -0,0 +1,1 @@
+export var unfinished_msg = "Sorry, but this part of the webite isn't finished yet and is undergoing heavy maintainence. As a side effect, parts may not work. Sorry for any inconvenience caused";
