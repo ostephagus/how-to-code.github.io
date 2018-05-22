91741379c2de1bffd4144e092c3d3be2f8207ddc
Sam <37499766+ostephagus@users.noreply.github.com>
2018-05-22 11:07:59 +0100

    Added a sleep function to hopefully reduce lag...

--- js-files/sam.js
+++ js-files/sam.js
@@ -1,7 +1,15 @@
 //Hide the nav menu then show it when mouse is hovered
 $('nav').hide();
-$('.menu').on('mouseenter', () => {
-	$('nav').fadeIn(500);
-}).on('mouseleave', () => {
-	$('nav').fadeOut(500);
-});
+function navA() {
+	window.setTimeOut(navB,1000)
+}
+function navB() {
+	$('.menu').on('mouseenter', () => {
+		$('nav').fadeIn(500);
+	}).on('mouseleave', () => {
+		$('nav').fadeOut(500);
+	});
+}
+while true {
+	navB()
+}
