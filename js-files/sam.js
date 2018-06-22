fad69fc8ad96bff37dfa1843e62fe18e801ffc9b
Sam <37499766+ostephagus@users.noreply.github.com>
2018-06-22 11:24:28 +0100

    1.1.8

--- js-files/sam.js
+++ js-files/sam.js
@@ -1,12 +1,10 @@
 //Hide the nav menu then show it when mouse is hovered
-console.log('JavaScript file running at version 1.1.7');
-$('nav').hide();
-window.setInterval(handleNav, 2000)
-
-function handleNav() {
+console.log('JavaScript file running at version 1.1.8');
+$(document).ready(() => {
+	$('nav').hide();
 	$('.menu').on('mouseenter', () => {
 		$('nav').fadeIn(500);
 	}).on('mouseleave', () => {
 		$('nav').fadeOut(500);
 	});
-}
+});
