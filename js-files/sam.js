77ed357824de4e454fe4ddc4a0d3ae3fb34c0398
Sam <37499766+ostephagus@users.noreply.github.com>
2018-09-05 11:18:07 +0100

    Update sam.js

--- js-files/sam.js
+++ js-files/sam.js
@@ -2,7 +2,7 @@
 console.log('JavaScript file running at version 1.1.8');
 $(document).ready(() => {
 	$('nav').hide();
-	$('.menu').on('mouseenter', () => {
+	$('.menu').on('click', () => {
 		$('nav').fadeIn(500);
 	}).on('mouseleave', () => {
 		$('nav').fadeOut(500);
