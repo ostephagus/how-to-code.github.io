70ffd278b1c505ac33d231b7cd70d73388983828
Sam <37499766+ostephagus@users.noreply.github.com>
2018-10-14 16:34:03 +0100

    Deleted the bugged setTimeOut((){}) statement

--- js-files/sam.js
+++ js-files/sam.js
@@ -1,13 +1,11 @@
 //Hide the nav menu then show it when mouse is hovered
-console.log('JavaScript file running at version 1.2.1');
+console.log('JavaScript file running at version 1.2.2');
 $(document).ready(() => {
 	$('nav').hide();
 	$('.menu').on('click', () => {
 		$('nav').fadeToggle(500);
 	});
-	setTimeout(function () {
-	        $("#MaintenanceBanner").fadeIn(200);
-     	}, 4000);
+	$("#MaintenanceBanner").fadeIn(200);
 	$("#closeMaintenanceBanner, .maintenanceBannerOK").click(function() {
 		$("#maintenanceBanner").fadeOut(200);
 	}); 
