004753046273822b71009cdb9d1836012e6047b1
Sam <37499766+ostephagus@users.noreply.github.com>
2018-10-19 16:27:43 +0100

    Update sam.js

--- js-files/sam.js
+++ js-files/sam.js
@@ -1,9 +1,16 @@
 console.log('JavaScript file running at version 1.2.3');
+let navBarClosed = true;
 $(document).ready(() => {
 	$('nav').hide();
 	$("#MaintenanceBanner").fadeIn(200);
 	$('.menu').on('click', () => {
-		$('nav').fadeToggle(500);
+		if (navBarClosed) {
+			$('.menu-button').text('Close');
+			$('nav').fadeToggle(300);
+		} else {
+			$('.menu-button').text('Menu');
+			$('nav').fadeToggle(300);
+		}
 	});
 	$("#closeMaintenanceBanner, .maintenanceBannerOK").on('click', () => {
 		$("#maintenanceBanner").fadeOut(200);
